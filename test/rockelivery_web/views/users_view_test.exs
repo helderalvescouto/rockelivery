2f68602de99176148e604e4d94b0d6ddee045720
Helder Couto <helder@brassun.com.br>
2022-01-27 00:08:34 -0300

    Testando nossa view

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,33 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created!",
+      user: %Rockelivery.Users.User{
+        address: "Rua das bananeiras, 25",
+        age: 27,
+        cep: "38408250",
+        cpf: "12345678901",
+        email: "vanessa@banana.com",
+        id: "f07486e0-5a2c-4740-b50a-2423a29da05a",
+        inserted_at: nil,
+        name: "Vanessa da Silva",
+        password: "123456",
+        password_hash: nil,
+        updated_at: nil
+      }
+    }
+
+    assert response = expected_response
+  end
+end
