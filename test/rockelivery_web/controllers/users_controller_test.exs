a3effa850aa146339ddf4021e702d94847f654b4
Helder Couto <helder@brassun.com.br>
2022-01-26 23:57:23 -0300

    Testando nosso controller de usuario 2

--- test/rockelivery_web/controllers/users_controller_test.exs
+++ test/rockelivery_web/controllers/users_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule RockeliveryWeb.UsersControllerTest do
   use RockeliveryWeb.ConnCase, async: true
 
+  import Rockelivery.Factory
+
   describe "create/2" do
     test "when all params are valid, returns the user", %{conn: conn} do
       params = %{
@@ -46,6 +48,21 @@ defmodule RockeliveryWeb.UsersControllerTest do
       expected_response = %{"message" => %{"address" => ["can't be blank"], "age" => ["can't be blank"], "cep" => ["can't be blank"], "cpf" => ["can't be blank"], "email" => ["can't be blank"]}}
 
       assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "when there is a user with the given id, deletes the user", %{conn: conn} do
+      id = "f07486e0-5a2c-4740-b50a-2423a29da05a"
+
+      insert(:user)
+
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> response(:no_content)
+
+      assert response == ""
     end
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
 
+  alias Rockelivery.Users.User
   def user_params_factory do
     %{
       address: "Rua das bananeiras, 25",
@@ -10,6 +11,19 @@ defmodule Rockelivery.Factory do
       email: "vanessa@banana.com",
       password: "123456",
       name: "Vanessa da Silva"
+    }
+  end
+
+  def user_factory do
+    %User{
+      address: "Rua das bananeiras, 25",
+      age: 27,
+      cep: "38408250",
+      cpf: "12345678901",
+      email: "vanessa@banana.com",
+      password: "123456",
+      name: "Vanessa da Silva",
+      id: "f07486e0-5a2c-4740-b50a-2423a29da05a"
     }
   end
 end
